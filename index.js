48ff747fa7044d4c1c8d875fc9a8fcae8a03f14a
Sidharth B <sidharth_b@jabil.com>
2023-09-25 13:20:27 +0530

    frontend prd build

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 morgan.token("requestBody", (req, res) => {
   return JSON.stringify(req.body)
 })
