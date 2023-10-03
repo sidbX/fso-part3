37ce50cec293ed18f1d57ffdaac400b40e519e59
Sidharth B <sidharth_b@jabil.com>
2023-10-03 08:03:49 +0530

    commented unwanted code

--- mongo.js
+++ mongo.js
@@ -1,37 +1,35 @@
-const mongoose = require("mongoose")
+// const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log("give password as argument")
-  process.exit(1)
-}
+// if (process.argv.length < 3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const password = process.argv[2]
-const connectionString = `mongodb+srv://sidbx:${password}@cluster0.krnoznc.mongodb.net/phoneBookApp?retryWrites=true&w=majority`
+// const password = process.argv[2]
+// const connectionString = process.env.MONGODB_URI
 
-mongoose.set("strictQuery", false)
-mongoose.connect(connectionString)
+// mongoose.set('strictQuery', false)  .catch((err) => next(err)), next
+// mongoose.connect(connectionString)
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
+// })
 
-const PersonModel = mongoose.model("Person", personSchema)
+// const PersonModel = mongoose.model('Person', personSchema)
 
-if (process.argv.length === 5) {
-  const doc = new PersonModel({
-    name: process.argv[3],
-    number: process.argv[4],
-  })
-  doc.save().then((result) => {
-    console.log(`added ${result.name} number ${result.number} to phonebook`)
-    mongoose.connection.close()
-  })
-} else if (process.argv.length === 3) {
-  PersonModel.find({}).then((results) => {
-    results.forEach((result) => console.log(result))
-    mongoose.connection.close()
-  })
-}
-
-
+// if (process.argv.length === 5) {
+//   const doc = new PersonModel({
+//     name: process.argv[3],
+//     number: process.argv[4],
+//   })
+//   doc.save().then((result) => {
+//     console.log(`added ${result.name} number ${result.number} to phonebook`)
+//     mongoose.connection.close()
+//   })
+// } else if (process.argv.length === 3) {
+//   PersonModel.find({}).then((results) => {
+//     results.forEach((result) => console.log(result))
+//     mongoose.connection.close()
+//   })
+// }
